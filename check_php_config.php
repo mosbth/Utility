c7045069a977bcff370bbcc007f2c73250c88173
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-06-09 11:15:38 +0200

    --

--- check_php_config.php
+++ check_php_config.php
@@ -19,10 +19,43 @@
 // Show output from phpinfo() if enabled
 //
 if(phpinfo()) {
-	echo phpinfo();
+	$html = phpinfo();
+} else {
+	$html = "<p>phpinfo() is disabled.</p>"
 }
 
 
+
+// -------------------------------------------------------------------------------------------
+//
+// Create and print out the html-page
+//
+$title = "Test PHP-configuration, what works and whats not";
+$charset = "utf-8";
+$language = "en";
+ 
+$html = <<< EOD
+<!DOCTYPE html>
+<html lang="{$language}">
+<head>
+	<meta charset="{$charset}" />
+	<title>{$title}</title>
+ 	<style></style>
+	<!--[if IE]> 
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>		
+	<![endif]-->
+</head>
+<body>
+	{$html}
+</body>
+</html>
+EOD;
+ 
+ 
+// Print the header and page
+header("Content-Type: text/html; charset={$charset}");
+echo $html;
+exit;
 
 
 ?>
