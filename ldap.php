1c98cea111f8ef973a0d49161ff7d19bcaed761f
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-08-20 16:52:26 +0200

    Error tracking ldap password

--- ldap.php
+++ ldap.php
@@ -87,7 +87,7 @@ $submit 	= strip_tags(isset($_POST['submit']) ? $_POST['submit'] : '');
 $server 	= strip_tags(isset($_POST['server']) ? $_POST['server']: '');
 $basedn 	= ldap_escape(isset($_POST['basedn']) ? $_POST['basedn'] : '', false);
 $uid			= strip_tags(isset($_POST['uid']) ? $_POST['uid'] : '');
-$password	= strip_tags(ldap_escape(isset($_POST['password']) ? $_POST['password'] : ''));
+$password	= strip_tags(ldap_escape(isset($_POST['password']) ? urldecode($_POST['password']) : ''));
 $usetls		= strip_tags(isset($_POST['tls']) ? $_POST['tls'] : '');
 
 //$basedn 	= strip_tags($_POST['basedn']);
