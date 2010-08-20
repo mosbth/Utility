6971af1efab368644e40de1be4678925730010df
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-08-20 17:06:02 +0200

    Error tracking ldap password

--- ldap.php
+++ ldap.php
@@ -87,7 +87,7 @@ $submit 	= strip_tags(isset($_POST['submit']) ? $_POST['submit'] : '');
 $server 	= strip_tags(isset($_POST['server']) ? $_POST['server']: '');
 $basedn 	= ldap_escape(isset($_POST['basedn']) ? $_POST['basedn'] : '', false);
 $uid			= strip_tags(isset($_POST['uid']) ? $_POST['uid'] : '');
-$password	= strip_tags(ldap_escape(isset($_POST['password']) ? stripslashes($_POST['password']) : ''));
+$password	= strip_tags(ldap_escape((isset($_POST['password']) ? stripslashes($_POST['password']) : '')));
 $usetls		= strip_tags(isset($_POST['tls']) ? $_POST['tls'] : '');
 
 //$basedn 	= strip_tags($_POST['basedn']);
@@ -100,7 +100,7 @@ $bindStatus 		= "";
 $searchStatus 	= "";
 $passwordStatus = "";
 
-echo $_POST['password'] . "<br>" . urlencode($_POST['password']) . "<br>" . stripslashes($_POST['password']) . "<br>" . $password;
+echo $_POST['password'] . "<br>" . urlencode($_POST['password']) . "<br>" . stripslashes($_POST['password']) . "<br>" . utf8_decode($_POST['password']) . "<br>" . $password;
 
 
 
