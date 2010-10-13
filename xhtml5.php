898c9d0997e2445aaef3206502f5e902de877819
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-10-13 15:09:00 +0200

    Adding testcase for xhtml5

---
+++ xhtml5.php
@@ -0,0 +1,14 @@
+<?php
+$charset = "utf-8";
+$mime = (stristr($_SERVER["HTTP_ACCEPT"],"application/xhtml+xml")) ? "application/xhtml+xml" : "text/html";
+header("content-type:$mime;charset=$charset");
+?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title></title>
+  </head>
+  <body>
+   <p><ahref="">This page validates as XHTML5 on validator.nu</a>.</p>
+   <p><a href="source.php?file=xhtml5.php">Sourcecode</a></p>
+  </body>
+</html>
