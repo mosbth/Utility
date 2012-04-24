327c42cc8c28cd6faa618ed594fd76282e3dfcaa
Mikael Roos <mos@bth.se>
2012-04-24 14:05:11 +0200

    adding image conversion on the fly

--- image_conversion_on_the_fly/img.php
+++ image_conversion_on_the_fly/img.php
@@ -8,7 +8,7 @@
  *
  */
 set_time_limit(20);
-isset($_GET['src']) or die('Must set src.');
+isset($_GET['src']) or die('Must set src. <a href="img.php?source=higher.jpg">img.php?source=higher.jpg</a> or <a href="img.php?source=wider.jpg">img.php?source=wider.jpg</a>');
 
 // Source must be set or exit
 /*
