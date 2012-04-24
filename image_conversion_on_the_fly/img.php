e2da1680b2443cfe4b371ebf96baade554986f3d
Mikael Roos <mos@bth.se>
2012-04-24 14:25:47 +0200

    adding image conversion on the fly

--- image_conversion_on_the_fly/img.php
+++ image_conversion_on_the_fly/img.php
@@ -8,7 +8,7 @@
  *
  */
 $extensions = array('jpg', 'jpeg', 'png', 'bmp', 'gif');
-$valid = implode(', ', $extenstions);
+$valid = implode(', ', $extensions);
 $usage = <<<EOD
 <p>You must set the src attribute pointing to a valid image.
 For example: <a href="img.php?src=higher.jpg">img.php?src=higher.jpg</a> or <a href="img.php?src=wider.jpg">img.php?src=wider.jpg</a>.
