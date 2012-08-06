d065ed671e5e04fbf46fb286b81cc60eff82883a
Mikael Roos <mos@bth.se>
2012-08-06 10:07:46 +0200

    linking to images failed corrected.

--- source.php
+++ source.php
@@ -302,7 +302,11 @@ if(isset($_GET['file'])) {
 	// Display image if a valid image file
 	//
 	if(in_array($source_extension, $IMAGES)) {
-	  $source_imgSrc = $source_file; //empty("{$sourceSubDir}{$source_currentdir}") ? $source_file : "{$sourceSubDir}{$source_currentdir}/{$source_file}";
+	  if(empty($sourceSubDir) && empty($source_currentdir)) {
+	    $source_imgSrc = $source_file;
+	  } else {
+	    $source_imgSrc = "{$sourceSubDir}{$source_currentdir}/{$source_file}";
+	  }
 		$source_content = "<div style='overflow:auto;'><img src='{$source_imgSrc}' alt='[image not found]'></div>";
 
 	//
